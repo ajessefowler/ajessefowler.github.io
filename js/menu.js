bcc8b10138a651e8201071b6528ddd85c794ccc1
Jesse Fowler <ajessefowler@gmail.com>
2018-07-21 18:51:40 -0400

    Menu now vertical, add menu content

--- js/menu.js
+++ js/menu.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
 	document.getElementById('menubutton').addEventListener('click', function() {
 		if (!menuOpen) {
 			menuOpen = true;
+			fadeInMenuItems();
 			document.getElementById('menu').style.animation = 'menuIn .3s ease forwards';
 			document.getElementById('menumiddle').style.animation = 'fadeOut .3s ease forwards';
 			document.getElementById('menushade').style.animation = 'fadeIn .3s ease forwards';
@@ -11,6 +12,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
 			document.getElementById('menubottom').style.animation = 'menuBottomOpen .3s ease forwards';
 		} else {
 			menuOpen = false;
+			fadeOutMenuItems();
 			document.getElementById('menu').style.animation = 'menuOut .3s ease forwards';
 			document.getElementById('menumiddle').style.animation = 'fadeIn .3s ease forwards';
 			document.getElementById('menushade').style.animation = 'fadeOut .3s ease forwards';
@@ -19,3 +21,21 @@ document.addEventListener('DOMContentLoaded', function(event) {
 		}
 	});
 });
+
+function fadeInMenuItems() {
+	let i;
+	const nodes = document.getElementById('menu').getElementsByTagName('p');
+
+	for (i = 0; i < nodes.length; i++) {
+		nodes[i].style.animation = 'fadeIn .2s ease .2s forwards';
+	}
+}
+
+function fadeOutMenuItems() {
+	let i;
+	const nodes = document.getElementById('menu').getElementsByTagName('p');
+
+	for (i = 0; i < nodes.length; i++) {
+		nodes[i].style.animation = 'fadeOut .1s ease forwards';
+	}
+}
