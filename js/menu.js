157b770c274f6703ae0aacfb62234f7e7b038cbb
Jesse Fowler <ajessefowler@gmail.com>
2018-07-21 17:56:00 -0400

    Add menu and menushade

--- js/menu.js
+++ js/menu.js
@@ -4,12 +4,16 @@ document.addEventListener('DOMContentLoaded', function(event) {
 	document.getElementById('menubutton').addEventListener('click', function() {
 		if (!menuOpen) {
 			menuOpen = true;
+			document.getElementById('menu').style.animation = 'menuIn .3s ease forwards';
 			document.getElementById('menumiddle').style.animation = 'fadeOut .3s ease forwards';
+			document.getElementById('menushade').style.animation = 'fadeIn .3s ease forwards';
 			document.getElementById('menutop').style.animation = 'menuTopOpen .3s ease forwards';
 			document.getElementById('menubottom').style.animation = 'menuBottomOpen .3s ease forwards';
 		} else {
 			menuOpen = false;
+			document.getElementById('menu').style.animation = 'menuOut .3s ease forwards';
 			document.getElementById('menumiddle').style.animation = 'fadeIn .3s ease forwards';
+			document.getElementById('menushade').style.animation = 'fadeOut .3s ease forwards';
 			document.getElementById('menutop').style.animation = 'menuTopClose .3s ease forwards';
 			document.getElementById('menubottom').style.animation = 'menuBottomClose .3s ease forwards';
 		}
